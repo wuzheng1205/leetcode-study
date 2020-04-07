0565ed75211bf07234e69196b73c41543dc4d949
wuzheng731201027@qq.com <wuzheng731201027@qq.com>
2020-04-07 21:32:28 +0800

    旋转矩阵DONE

--- src/main/java/com/jax/study/leetcode/medium/旋转矩阵.java
+++ src/main/java/com/jax/study/leetcode/medium/旋转矩阵.java
@@ -22,24 +22,28 @@ public class 旋转矩阵 {
     }
 
     static class Solution {
+
+        /**
+         * 原地旋转，每次顺时针交换4个点的值
+         */
         public void rotate(int[][] matrix) {
             int x = matrix.length;
             for (int i = 0; i < matrix.length / 2; i++) {
                 for (int j = i; j < matrix[i].length - i - 1; j++) {
+                    int a1 = i, b1 = j;
+                    int a2 = b1, b2 = x - a1 - 1;
+                    int a3 = b2, b3 = x - a2 - 1;
+                    int a4 = b3, b4 = x - a3 - 1;
                     // 交换
-                    int tmp = matrix[i][j];//第1个点
+                    int tmp = matrix[a1][b1];//第1个点
                     // 第4个点赋值给第1个点
-                    matrix[i][j] = matrix[j][i];// 第4个点
+                    matrix[a1][b1] = matrix[a4][b4];// 第4个点
                     // 第3个点赋值给第4个点
-                    matrix[j][i] = matrix[x - i - 1][j];
+                    matrix[a4][b4] = matrix[a3][b3];
                     // 第2个点赋值给第3个点
-                    matrix[x - i - 1][j] = matrix[j][x - i - 1];
+                    matrix[a3][b3] = matrix[a2][b2];
                     // 第1个点赋值给第2个点
-                    matrix[j][x - i - 1] = tmp;
-//                    matrix[i][j] = matrix[j][x - i - 1];// 第2个点
-//                    matrix[j][x - i - 1] = matrix[x - i - 1][j];// 第3个点
-//                    matrix[x - i - 1][j] = matrix[j][i];// 第4个点
-//                    matrix[j][i] = tmp;
+                    matrix[a2][b2] = tmp;
                 }
             }
         }
